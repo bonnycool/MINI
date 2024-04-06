97981e54238c35e50331f5fc79833e2eff20d1f5
bonnycool <bonnydavid543@gmail.com>
2024-04-06 18:41:30 +0530

    chumma commit

--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -6,7 +6,8 @@ const Navbar = () => {
     <div>
       <h1>hello</h1>
       <h2>helllo</h2>
-      <AiOutlineMenu size={30}/>\
+      <AiOutlineMenu size={30}/>
+      
       <h4>i am in master not in testbranch</h4>
     </div>
   )
