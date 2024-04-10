42f57156216c94d5bec56875d8988f07c601555f
bonnycool <bonnydavid543@gmail.com>
2024-04-10 09:22:54 +0530

    spacing

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -6,7 +6,7 @@ const Login = () => {
     return (
         <div className="min-h-screen flex items-center justify-center" style={{backgroundImage: "url(../src/Assests/IMAGES/saintgitsbg.jpeg)"}}>
 
-<div className="bg-white p-8 rounded shadow-md w-full sm:w-96">
+        <div className="bg-white p-8 rounded shadow-md w-full sm:w-96">
 
                 {/* Login form will go here */}
             </div>
