6b86468a421b090e89029aa965fd13d9856d8533
bonnycool <bonnydavid543@gmail.com>
2024-04-12 13:47:21 +0530

    font size changeed of text in mobile

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -2,12 +2,15 @@ import React, { useState, useEffect } from 'react';
 
 const Header = () => {
     const [isMobileScreen, setIsMobileScreen] = useState(false);
+    const [isTabletScreen, setIsTabletScreen] = useState(false);
     const maxGitsConnectLogoWidthVh = 10; // Adjust the percentage as needed for GitsConnect logo
     const maxSaintGitsLogoWidthVh = 30; // Adjust the percentage as needed for SaintGits logo
 
     useEffect(() => {
         const handleResize = () => {
-            setIsMobileScreen(window.innerWidth <= 768);
+            const screenWidth = window.innerWidth;
+            setIsMobileScreen(screenWidth <= 768);
+            setIsTabletScreen(screenWidth > 768 && screenWidth <= 1024);
         };
 
         // Add event listener for window resize
@@ -26,13 +29,20 @@ const Header = () => {
         <header className="flex justify-between items-center p-2 text-white" style={{ backgroundColor: "#fff7ed", height: "auto", minHeight: "40px", position: "fixed", top: "0", left: "0", right: "0", zIndex: "1000" }}>
             <div className="flex items-center">
                 <img src="../src/Assests/IMAGES/gitsconnectlogo.jpeg" alt="GitsConnect Logo" className="mr-2" style={{ height: "auto", maxWidth: `${maxGitsConnectLogoWidthVh}vh`, maxHeight: "100%", marginRight: "10px" }} />
-                <h1 className="text-lg font-bold" style={{ color: "#d1d5db", textShadow: "1px 1px 2px rgba(0,0,0,0.2)", fontSize: "3vw", whiteSpace: "nowrap" }}>GITSCONNECT</h1>
+                {/* Render GitsConnect text */}
+                {!isMobileScreen && (
+                    <h1 className="text-lg font-bold" style={{ color: "#d1d5db", textShadow: "1px 1px 2px rgba(0,0,0,0.2)", fontSize: isTabletScreen ? "3.5vw" : "3.5vw", whiteSpace: "nowrap", marginLeft: isTabletScreen ? "5px" : "0" }}>GITSCONNECT</h1>
+                )}
             </div>
-            {/* Conditionally render SaintGits logo based on screen size */}
+            {/* Render SaintGits logo only for desktop view */}
             {!isMobileScreen && (
                 <div className="flex items-center">
                     <img src="../src/Assests/IMAGES/saintgitslogo.png" alt="SaintGits Logo" className="mr-2" style={{ height: "auto", maxWidth: `${maxSaintGitsLogoWidthVh}vh`, maxHeight: "100%" }} />
                 </div>
+            )}
+            {/* Conditionally render GitsConnect text in the middle for mobile view */}
+            {isMobileScreen && (
+                <h1 className="text-2xl font-bold" style={{ color: "#d1d5db", textShadow: "1px 1px 2px rgba(0,0,0,0.2)", whiteSpace: "nowrap", flex: "1", textAlign: "center", marginLeft: "10px" }}>GITSCONNECT</h1>
             )}
         </header>
     );
