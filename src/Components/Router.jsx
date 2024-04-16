39babbda11edafc778912c8ceba63c4f030f16d3
bonnycool <bonnydavid543@gmail.com>
2024-04-16 23:34:25 +0530

    Interface route created

--- src/Components/Router.jsx
+++ src/Components/Router.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Login from "../pages/login"; // Import the Login component
+import Interface from "../pages/interface";
 export function RouterPaths() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/interface" element={<Interface />} />
       {/* Add more routes as needed */}
     </Routes>
   );
---
+++ src/pages/interface.jsx
@@ -0,0 +1,15 @@
+
+
+import Header from "../Components/header";
+
+const Interface = () => {
+  return (
+    <div >
+      <Header />
+      <h2>helloS</h2>
+      {/* Add the rest of your page components here */}
+    </div>
+  );
+};
+
+export default Interface;
